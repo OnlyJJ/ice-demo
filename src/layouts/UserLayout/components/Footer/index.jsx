43e007a681bd0997dde724e15bfdfa1781c8223b
邵翔 <370083084@qq.com>
2019-08-26 18:12:20 +0800

    +

--- src/layouts/UserLayout/components/Footer/index.jsx
+++ src/layouts/UserLayout/components/Footer/index.jsx
@@ -5,17 +5,9 @@ export default () => {
   return (
     <div className={styles.footer}>
       <div className={styles.links}>
-        <a href="#" className={styles.link}>
-          帮助
-        </a>
-        <a href="#" className={styles.link}>
-          隐私
-        </a>
-        <a href="#" className={`${styles.link} ${styles.contract}`}>
-          条款
-        </a>
+       
       </div>
-      <div className={styles.copyright}>阿里巴巴集团 © 2018 版权所有</div>
+      <div className={styles.copyright}></div>
     </div>
   );
 };
--- src/layouts/UserLayout/index.jsx
+++ src/layouts/UserLayout/index.jsx
@@ -5,15 +5,12 @@ import styles from './index.module.scss';
 
 export default function UserLayout(props) {
   return (
-    <Layout className={styles.userLayout}>
-      <div className={styles.header}>
-        <a href="#" className={styles.meta}>
-          <span className={styles.title}>LOGO</span>
-        </a>
-        <p className={styles.desc}>让前端开发简单而友好</p>
-      </div>
+    /**
+    * 这里不使用任何样式，样式依赖登录页本身的样式
+    */
+    <Layout >
       {props.children}
-      <Footer />
+    <Footer />
     </Layout>
   );
 }
