5ecee9fb9fd79be0698b0da44cae6a2d3f80fc11
邵翔 <370083084@qq.com>
2019-08-26 18:14:11 +0800

    +

--- mock/index.js
+++ mock/index.js
@@ -2,9 +2,9 @@ module.exports = {
   'GET /api/profile': {
     status: 'SUCCESS',
     data: {
-      name: '淘小宝',
-      department: '技术部',
-      avatar: 'https://img.alicdn.com/tfs/TB1L6tBXQyWBuNjy0FpXXassXXa-80-80.png',
+      name: '管理员',
+      department: '',
+      avatar: 'yisheng.png',
       userid: 10001,
     },
   },
